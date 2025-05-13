29af360ac38e47138b18bd59cd84d1096481cf57
Devlin <rewlinofficial@gmail.com>
2025-05-12 21:16:26 -0700

    update main.py
    debugging

--- backend/main.py
+++ backend/main.py
@@ -7,27 +7,30 @@ from screens.profile_screen import ProfileScreen
 from screens.rewards_screen import RewardsScreen
 from screens.settings_screen import SettingsScreen
 from screens.habits_screen import HabitsScreen
+from screens.memento_mori_screen import MementoMoriScreen
 import traceback
 
 def get_theme(theme_mode=ft.ThemeMode.DARK):
     is_dark = theme_mode == ft.ThemeMode.DARK
     return ft.Theme(
         color_scheme=ft.ColorScheme(
-            primary=ft.colors.AMBER,
-            secondary=ft.colors.DEEP_PURPLE,
-            surface=ft.colors.GREY_900 if is_dark else ft.colors.GREY_100,
-            on_surface=ft.colors.WHITE if is_dark else ft.colors.BLACK,
-            background=ft.colors.GREY_900 if is_dark else ft.colors.GREY_50,
-            surface_variant=ft.colors.GREY_800 if is_dark else ft.colors.GREY_200,
-        ),
-        text_theme=ft.TextTheme(
-            body_large=ft.TextStyle(font_family="Cinzel"),
-            title_large=ft.TextStyle(font_family="Cinzel", weight="bold")
+            primary=ft.Colors.BLUE_200,
+            secondary=ft.Colors.DEEP_PURPLE,
+            surface=ft.Colors.GREY_900 if is_dark else ft.Colors.GREY_100,
+            on_surface=ft.Colors.WHITE if is_dark else ft.Colors.BLACK,
+            background=ft.Colors.GREY_900 if is_dark else ft.Colors.GREY_50,
+            surface_variant=ft.Colors.GREY_800 if is_dark else ft.Colors.GREY_200,
+            on_primary_container=ft.Colors.BLUE_200,
+            primary_container=ft.Colors.BLUE_200,
         ),
     )
 
 def main(page: ft.Page):
     try:
+        page.window.width = 1400
+        page.window.height = 1000
+        page.window.resizable = True
+        page.window.center()
         page.theme = get_theme(page.theme_mode)
         page.fonts = {
             "Cinzel": "https://fonts.googleapis.com/css2?family=Cinzel:wght@400;700&display=swap"
@@ -39,14 +42,15 @@ def main(page: ft.Page):
         
         player = Player()
         
-        # pass page to MapScreen
         home_screen = HomeScreen(player)
         quests_screen = QuestsScreen(player)
-        map_screen = MapScreen(player, page)  # pass page here
+        map_screen = MapScreen(player, page)
         profile_screen = ProfileScreen(player)
         rewards_screen = RewardsScreen(player)
-        habits_screen = HabitsScreen(player) 
+        habits_screen = HabitsScreen(player, page)  
         settings_screen = SettingsScreen(player, page)
+        memento_mori_screen = MementoMoriScreen()
+
         
         screens = [
             home_screen,
@@ -55,20 +59,19 @@ def main(page: ft.Page):
             profile_screen,
             rewards_screen,
             habits_screen,
+            memento_mori_screen,
             settings_screen
         ]
         
-        # set the initial screen
         current_screen = ft.AnimatedSwitcher(
             transition=ft.AnimatedSwitcherTransition.FADE,
-            duration=200,
+            duration=50,
             switch_in_curve=ft.AnimationCurve.EASE_IN,
             switch_out_curve=ft.AnimationCurve.EASE_OUT,
             content=home_screen,
             expand=True
         )
         
-        # changing tabs
         def change_tab(e):
             selected_index = e.control.selected_index
             if selected_index < len(screens):
@@ -77,49 +80,53 @@ def main(page: ft.Page):
                 current_screen.update()
                   
         
-        # navigation rail
         rail = ft.NavigationRail(
         selected_index=0,
         label_type=ft.NavigationRailLabelType.ALL,
         min_width=100,
         min_extended_width=200,
-        bgcolor=ft.colors.SURFACE_VARIANT,
-        indicator_color=ft.colors.AMBER,
+        bgcolor=ft.Colors.ON_SURFACE_VARIANT,
+        indicator_color=ft.Colors.AMBER,
         destinations=[
-        ft.NavigationRailDestination(
-            icon=ft.icons.HOME_OUTLINED, 
-            selected_icon=ft.icons.HOME, 
-            label="Home"
-        ),
-        ft.NavigationRailDestination(
-            icon=ft.icons.TASK,
-            selected_icon=ft.icons.TASK_OUTLINED,
-            label="Quests"
-        ),
-        ft.NavigationRailDestination(
-            icon=ft.icons.MAP,
-            selected_icon=ft.icons.MAP_OUTLINED,
-            label="Map"
-        ),
-        ft.NavigationRailDestination(
-            icon=ft.icons.PERSON,
-            selected_icon=ft.icons.PERSON_OUTLINED,
-            label="Profile"
-        ),
-        ft.NavigationRailDestination(
-            icon=ft.icons.SHOP,
-            selected_icon=ft.icons.SHOP_OUTLINED,
-            label="Rewards"
-        ),
-        ft.NavigationRailDestination(
-            icon=ft.icons.CHECK_CIRCLE,
-            selected_icon=ft.icons.CHECK_CIRCLE_OUTLINE,
-            label="Habits"
-        ),
-        ft.NavigationRailDestination(
-            icon=ft.icons.SETTINGS,
-            selected_icon=ft.icons.SETTINGS_OUTLINED,
-            label="Settings"
+    ft.NavigationRailDestination(
+        icon=ft.Icon(name=ft.Icons.HOME_OUTLINED, color=ft.Colors.WHITE),
+        selected_icon=ft.Icon(name=ft.Icons.HOME, color=ft.Colors.AMBER_300),
+        label="Home"
+    ),
+    ft.NavigationRailDestination(
+        icon=ft.Icon(name=ft.Icons.TASK_OUTLINED, color=ft.Colors.WHITE),
+        selected_icon=ft.Icon(name=ft.Icons.TASK, color=ft.Colors.AMBER_300),
+        label="Quests"
+    ),
+    ft.NavigationRailDestination(
+        icon=ft.Icon(name=ft.Icons.MAP_OUTLINED, color=ft.Colors.WHITE),
+        selected_icon=ft.Icon(name=ft.Icons.MAP, color=ft.Colors.AMBER_300),
+        label="Map"
+    ),
+    ft.NavigationRailDestination(
+        icon=ft.Icon(name=ft.Icons.PERSON_OUTLINED, color=ft.Colors.WHITE),
+        selected_icon=ft.Icon(name=ft.Icons.PERSON, color=ft.Colors.AMBER_300),
+        label="Profile"
+    ),
+    ft.NavigationRailDestination(
+        icon=ft.Icon(name=ft.Icons.SHOP_OUTLINED, color=ft.Colors.WHITE),
+        selected_icon=ft.Icon(name=ft.Icons.SHOP, color=ft.Colors.AMBER_300),
+        label="Rewards"
+    ),
+    ft.NavigationRailDestination(
+        icon=ft.Icon(name=ft.Icons.CHECK_CIRCLE_OUTLINE, color=ft.Colors.WHITE),
+        selected_icon=ft.Icon(name=ft.Icons.CHECK_CIRCLE, color=ft.Colors.AMBER_300),
+        label="Habits"
+    ),
+    ft.NavigationRailDestination(
+        icon=ft.Icon(name=ft.Icons.CALENDAR_MONTH_OUTLINED, color=ft.Colors.WHITE),
+        selected_icon=ft.Icon(name=ft.Icons.CALENDAR_MONTH, color=ft.Colors.AMBER_300),
+        label="Memento Mori"
+    ),
+    ft.NavigationRailDestination(
+        icon=ft.Icon(name=ft.Icons.SETTINGS_OUTLINED, color=ft.Colors.WHITE),
+        selected_icon=ft.Icon(name=ft.Icons.SETTINGS, color=ft.Colors.AMBER_300),
+        label="Settings"
         ),
     ],
     on_change=change_tab
@@ -145,4 +152,4 @@ def main(page: ft.Page):
         page.update()
         print(f"Error: {e}\n{traceback.format_exc()}")
 
-ft.app(target=main)
+ft.app(target=main, view=ft.AppView.FLET_APP)
